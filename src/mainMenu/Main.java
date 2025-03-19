087896754343e4b945d7e4b7c703b2148a75294a
Mateusz Mikula <mikula@onet.com.pl>
2025-03-19 23:11:39 +0100

    space

--- src/mainMenu/Main.java
+++ src/mainMenu/Main.java
@@ -10,6 +10,7 @@ import java.nio.file.Paths;
 
 public class Main extends Application {
 
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("mainmenu.fxml"));
